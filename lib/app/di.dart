8d4985f07d413a990919717cbae1460d729e8440
Francisco <fp226766@gmail.com>
2025-09-18 16:11:34 +0100

    fake di file

---
+++ lib/app/di.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+
+class _AuthRepositoryFake {
+  bool isLoggedIn = false;
+  final _ctrl = StreamController<void>.broadcast();
+
+  Stream<void> get authStateChanges => _ctrl.stream;
+  void signIn() { isLoggedIn = true; _ctrl.add(null); }
+  void signOut() { isLoggedIn = false; _ctrl.add(null); }
+}
+
+class _DI {
+  late final _AuthRepositoryFake authRepository;
+  Future<void> init() async {
+    authRepository = _AuthRepositoryFake();
+  }
+}
+
+final di = _DI();
