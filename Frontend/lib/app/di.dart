aa5b7dfef72ebcf1cfe4fb420685c1464cb99aba
Francisco <fp226766@gmail.com>
2025-09-21 23:10:07 +0100

    put the authrepository on the di file

--- Frontend/lib/app/di.dart
+++ Frontend/lib/app/di.dart
@@ -1,24 +1,12 @@
-import 'package:supabase_flutter/supabase_flutter.dart';
-import '../core/env.dart';
 import '../data/repositories/auth_repository.dart';
 
-class DI {
-  DI._();
-  static final DI I = DI._();
+final di = _DI();
 
-  late final SupabaseClient supabase;
+class _DI {
   late final AuthRepository authRepository;
 
   Future<void> init() async {
-    final supa = await Supabase.initialize(
-      url: Env.supabaseUrl,
-      anonKey: Env.supabaseAnonKey,
-    );
-    supabase = supa.client;
-
-    authRepository = AuthRepository(supabase);
+    authRepository = AuthRepository();
+    await authRepository.bootstrap(); // lê tokens guardados
   }
 }
-
-// Exporta um atalho global
-final di = DI.I;
