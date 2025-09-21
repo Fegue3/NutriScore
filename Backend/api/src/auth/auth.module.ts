1baced9a58b90a5bef12d0a28d1a9ae72b414be3
Francisco <fp226766@gmail.com>
2025-09-21 20:58:49 +0100

    added all the imports

---
+++ Backend/api/src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
+import { PrismaModule } from '../prisma/prisma.module';
+import { JwtAccessStrategy, JwtRefreshStrategy } from './auth.strategies';
+
+@Module({
+  imports: [
+    UsersModule,
+    PrismaModule,
+    PassportModule,
+    JwtModule.register({}), // vamos passar secret/ttl por chamada
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtAccessStrategy, JwtRefreshStrategy],
+})
+export class AuthModule {}
