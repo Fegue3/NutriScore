6ea4cc5ad11ce31abea911679222e7680fcc1a25
Francisco <fp226766@gmail.com>
2025-09-21 20:58:06 +0100

    added and fixed the controller app

--- Backend/api/src/app.controller.ts
+++ Backend/api/src/app.controller.ts
@@ -1,16 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get('health')
   health() {
-    return {
-      status: 'ok',
-      message: 'API is running!',
-      timestamp: new Date().toISOString(),
-    };
+    return { ok: true };
   }
 }
