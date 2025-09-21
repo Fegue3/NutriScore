bda7ffc92f11e3990546d1ce34e81f464643965d
Francisco <fp226766@gmail.com>
2025-09-21 20:59:26 +0100

    added user service

---
+++ Backend/api/src/users/users.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  findByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+
+  findById(id: string) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+
+  async createUser(email: string, passwordHash: string, name?: string | null): Promise<User> {
+    const user = await this.prisma.user.create({
+      data: { email, passwordHash, name: name ?? null },
+    });
+    await this.prisma.userPreferences.create({ data: { userId: user.id } }).catch(() => {});
+    await this.prisma.userGoals.create({ data: { userId: user.id } }).catch(() => {});
+    return user;
+  }
+
+  updateRefreshHash(userId: string, refreshTokenHash: string | null) {
+    return this.prisma.user.update({ where: { id: userId }, data: { refreshTokenHash } });
+  }
+}
