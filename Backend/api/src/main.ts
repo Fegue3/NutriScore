f3fd6230eab93b692b69cf0c44227d045d08d693
Francisco <fp226766@gmail.com>
2025-09-22 23:38:00 +0100

    added transform true

--- Backend/api/src/main.ts
+++ Backend/api/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: process.env.CORS_ORIGIN ?? '*', credentials: true });
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true,transform: true, forbidNonWhitelisted: true }));
   await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
 }
 bootstrap();
