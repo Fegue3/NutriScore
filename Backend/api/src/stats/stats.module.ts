1ad47fdc84a48f0c53ed1bfa0306637fedc1a71d
Francisco <fp226766@gmail.com>
2025-10-08 00:33:11 +0100

    removed comment

--- Backend/api/src/stats/stats.module.ts
+++ Backend/api/src/stats/stats.module.ts
@@ -1,12 +1,13 @@
+// src/stats/stats.module.ts
 import { Module } from '@nestjs/common';
-import { PrismaModule } from '../prisma/prisma.module';
 import { StatsController } from './stats.controller';
 import { StatsService } from './stats.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
   controllers: [StatsController],
   providers: [StatsService],
-  exports: [StatsService], // para MealsService injetar
+  exports: [StatsService],
 })
 export class StatsModule {}
