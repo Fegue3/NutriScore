1dc6635722f02e1ed91c46e7fccbb99a16f92712
Francisco <fp226766@gmail.com>
2025-10-09 23:07:24 +0100

    added the calories module import

--- Backend/api/src/stats/stats.module.ts
+++ Backend/api/src/stats/stats.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { StatsController } from './stats.controller';
 import { StatsService } from './stats.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { CaloriesModule } from '../calories/calories.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CaloriesModule],
   controllers: [StatsController],
   providers: [StatsService],
   exports: [StatsService],
