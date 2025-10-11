51a3d5fc4fdb325fe0abc17d5794047d8bcb5d7a
Francisco <fp226766@gmail.com>
2025-10-11 16:13:09 +0100

    support for all ports

--- Backend/api/src/main.ts
+++ Backend/api/src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: process.env.CORS_ORIGIN ?? '*', credentials: true });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true,transform: true, forbidNonWhitelisted: true }));
-  await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
+  await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000, '0.0.0.0');
 }
 bootstrap();
